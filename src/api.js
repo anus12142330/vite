6509d4a2603e140d36a9cc743b1f1ec6de694868
anus12142330 <49470936+anus12142330@users.noreply.github.com>
2025-09-14 16:02:21 +0400

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,1 +1,5 @@
 
+// src/api.js
+import axios from 'axios';
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE || 'http://localhost:5641';
+axios.defaults.withCredentials = true; // if you use sessions/cookies
